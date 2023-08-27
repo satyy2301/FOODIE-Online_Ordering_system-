f2d37bca7c96183d90d5a49e44f138c7a5f2efcc
aryan2702p <arynmi02@gmail.com>
2023-08-27 23:58:06 +0530

    Day-2 Created login and signup page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "../features/NavBar/Navbar";
+import ProductList from "../features/product_list/ProductList";
+
+function Home() {
+    return ( 
+        <Navbar>
+            <ProductList />
+               
+           
+        </Navbar>
+     );
+}
+
+export default Home;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import { Login } from "../features/auth/components/Login";
+function LoginPage() {
+    return (
+        <Login />
+     );
+}
+
+export default LoginPage;
